125925ed8f27ba5cff5685e1c676900bcab355ae
geeves <geeves@users.noreply.github.com>
2022-05-11 21:08:16 -0400

    Update dependencies (#5)
    * Update all dependencies
    * Remove ESLint for now (it may return, but it is causing more issues than it solves at the moment)
    * Add padding to radio button group
    * Remove uglify and extraneous stuff from webpack
    * Change from  space to tab for visual accessibility

--- src/index.js
+++ src/index.js
@@ -4,23 +4,23 @@ import CamelotWheel from "./components/CamelotWheel";
 import "./components/camelot.css";
 
 const App = () => (
-  <div id="camelot-container">
-    <CamelotWheel
-      spacing="16"
-      classes={{control: "control"}}/>
-  </div>
+	<div id="camelot-container">
+		<CamelotWheel
+			spacing="16"
+			classes={{control: "control"}}/>
+	</div>
 );
 
 const renderCamelotWheel = () => {
-  render(
-    <React.Fragment>
-      <App/>
-    </React.Fragment>,
-    document.getElementById("main")
-  );
+	render(
+		<React.Fragment>
+			<App/>
+		</React.Fragment>,
+		document.getElementById("main")
+	);
 };//
 
 
 window.addEventListener("DOMContentLoaded", () => {
-  renderCamelotWheel();
+	renderCamelotWheel();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,40 @@
 const path = require("path");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 const MAIN_DIR = path.resolve(__dirname);
 const SRC_DIR = path.resolve(__dirname, "src");
 
 const config = {
-  entry : {
-    "cw" : `${SRC_DIR}/index.js`
-  },
-  output : {
-    path : `${MAIN_DIR}/dist`,
-    filename : "[name].js"
-  },
-  module : {
-    rules : [
-      {
-        test : /\.js?/,
-        include : [SRC_DIR],
-        loader : "babel-loader"
-      },
-      {
-        test: /\.svg$/,
-        loader: "svg-inline-loader"
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  plugins : []
+	entry: {
+		"cw": `${SRC_DIR}/index.js`
+	},
+	output: {
+		path: `${MAIN_DIR}/dist`,
+		filename: "[name].js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?/,
+				include: [SRC_DIR],
+				loader: "babel-loader"
+			},
+			{
+				test: /\.svg$/,
+				loader: "svg-inline-loader"
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			}
+		]
+	},
+	plugins: []
 };
 
 module.exports = (env, argv) => {
-  if ("production" === argv.mode) {
-    // config.optimization = {
-    //   minimizer : [
-    //     new UglifyJSPlugin({
-    //       uglifyOptions : {
-    //         compress : {
-    //           drop_console : true
-    //         }
-    //       }
-    //     })
-    //   ]
-    // };
-  } else {
-    config.devtool = "eval-source-map";
-    config.output.filename = "[name]-development.js";
-  }
-  return config;
+	if ("production" !== argv.mode) {
+		config.devtool = "eval-source-map";
+		config.output.filename = "[name]-development.js";
+	}
+	return config;
 };
